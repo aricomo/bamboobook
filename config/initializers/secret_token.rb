36fc48b62a16e106d5f3e5ac8063b557768de63f
ariko komoda <arikokomoda@gmail.com>
2015-06-11 17:36:54 -0700

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bamboobook::Application.config.secret_token = '2f554680cee821a7aa54fd7023c73ee6ab61c3eda7ed936200de6c85b8e883e6dab660090f6b2f4edf5faf84255fbeb2161336ae9de97369d2fcab72615acbe8'
